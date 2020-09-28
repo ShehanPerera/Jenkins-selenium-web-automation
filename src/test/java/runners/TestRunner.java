9d678726c81f3fdc169283d419b657b8ece957f0
shehanp <shehanp@hsenidmobile.com>
2020-09-28 22:29:14 +0530

    update project structure and add html report

---
+++ src/test/java/runners/TestRunner.java
@@ -0,0 +1,26 @@
+package runners;
+
+import com.cucumber.listener.Reporter;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+import java.io.File;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue= {"src/seleniumgluecode"},
+        plugin = { "pretty","com.cucumber.listener.ExtentCucumberFormatter:/home/shehan/IdeaProjects/mSpaceSeleniumCucumber" +
+                "/extentReports/extentReports.html","html:/home/shehan/IdeaProjects/mSpaceSeleniumCucumber" +
+                "/htmlReports/"}
+)
+public class TestRunner {
+    @AfterClass
+    public static void writeExtentReport() {
+        Reporter.loadXMLConfig(new File("config/report.xml"));
+
+    }
+}
+
